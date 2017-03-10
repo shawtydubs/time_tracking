30c682219bd15c6f254a740ffbb009055b2c73ae
shawtydubs <shawtydubs@users.noreply.github.com>
2017-03-10 00:34:10 -0500

    Initial version of time tracking Chrome extension

---
+++ app/timeContoller.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const SUBMIT_TIME = 60;
+
+angular.module('timeTracking').controller('timeController', function ($scope, timeStorage) {
+    $scope.timeStorage = timeStorage;
+
+    $scope.projectsWithTime = {};
+
+    // project functions
+
+    $scope.$watch('timeStorage.projectData', function() {
+        $scope.projectList = $scope.timeStorage.projectData;
+    });
+
+    $scope.timeStorage.getProjects(function(data) {
+        $scope.projectList = data;
+        $scope.$apply();
+    });
+ 
+    $scope.addProject = function() {
+        console.log('Adding project: ' + $scope.newProject);
+        timeStorage.addProject($scope.newProject);
+        $scope.newProject = '';
+    }
+
+    $scope.removeProject = function(project) {
+        timeStorage.removeProject(project);
+    }
+
+    $scope.deleteAllProjects = function() {
+        console.log('Deleting all projects');
+        timeStorage.deleteAllProjects();
+    }
+
+    // timesheet functions
+
+    $scope.$watch('timeStorage.timesheetData', function() {
+        $scope.timesheetSummary = $scope.timeStorage.timesheetData;
+    });
+
+    $scope.timeStorage.getTimesheet(function(data) {
+        $scope.timesheetSummary = data;
+        $scope.$apply();
+    });
+
+    $scope.submitTime = function() {
+        for (var t in $scope.projectsWithTime) {
+            if ($scope.projectsWithTime.hasOwnProperty(t) && $scope.projectsWithTime[t] == false) {
+                delete $scope.projectsWithTime[t];
+            }
+        }
+
+        var minutes = SUBMIT_TIME / Object.keys($scope.projectsWithTime).length;
+        var date = '2017-03-11'; //moment().format('YYYY-MM-DD');
+        console.log(date);
+        var newTime = $scope.timesheetSummary;
+        if (!newTime.hasOwnProperty(date)) { newTime[date] = {} };
+        console.log(newTime);
+
+        for (var t in $scope.projectsWithTime) {
+            if (newTime[date].hasOwnProperty(t)) {
+                newTime[date][t] += minutes;
+            } else {
+                newTime[date][t] = minutes;
+            }
+        }
+
+        timeStorage.submitTimesheet(newTime);
+        $scope.projectsWithTime = {};
+    }
+
+    $scope.clearTimesheet = function() {
+        console.log('Clearing timesheet');
+        timeStorage.clearTimesheet();
+    }
+
+    $scope.isEmpty = function(obj) {
+        if (Object.keys(obj).length == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+});
---
+++ app/timeStorage.js
@@ -0,0 +1,68 @@
+angular.module('timeTracking').service('timeStorage', function ($q) {
+    var _this = this;
+    
+    this.projectData = [];
+    this.timesheetData = {};
+
+    // project functions
+
+    this.getProjects = function(callback) {
+        chrome.storage.sync.get('project', function(keys) {
+            _this.projectData = keys.project;
+            console.group('Project data');console.log(_this.projectData);console.groupEnd();
+            callback(_this.projectData);
+        })
+    }
+
+    this.addProject = function(newProject) {
+        var id = this.projectData.length + 1;
+        var project = {
+            id: id,
+            name: newProject
+        };
+        this.projectData.push(project);
+        this.syncProject();
+    }
+
+    this.removeProject = function(project) {
+        this.projectData.splice(this.projectData.indexOf(project), 1);
+        this.syncProject();
+    }
+
+    this.deleteAllProjects = function() {
+        this.projectData = [];
+        this.syncProject();
+    }
+
+    this.syncProject = function() {
+        chrome.storage.sync.set({ project: this.projectData }, function() {
+            console.log('Project synced with Chrome');
+        });
+    }
+
+    // timesheet functions
+
+    this.getTimesheet = function(callback) {
+        chrome.storage.sync.get('timesheet', function(keys) {
+            _this.timesheetData = keys.timesheet;
+            console.group('Timesheet data');console.log(_this.timesheetData);console.groupEnd();
+            callback(_this.timesheetData);
+        })
+    }
+
+    this.submitTimesheet = function(newTime) {
+        this.timesheetData = newTime;
+        this.syncTimesheet();
+    }
+
+    this.syncTimesheet = function() {
+        chrome.storage.sync.set({ timesheet: this.timesheetData }, function() {
+            console.log('Timesheet synced with Chrome');
+        })
+    }
+
+    this.clearTimesheet = function() {
+        this.timesheetData = {};
+        this.syncTimesheet();
+    }
+});
